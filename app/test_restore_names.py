7b03f28d40eb8d7a7a74c9922ea0510b201e418b
Maksym Kryvsha <KryivshaMaxim@gmail.com>
2024-12-06 15:59:12 +0200

    solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,23 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "first_in, then_out",
+    [([{"first_name": None,
+        "last_name": "Holy",
+        "full_name": "Jack Holy"},
+       {"last_name": "Adams",
+        "full_name": "Mike Adams"}],
+      [{"first_name": "Jack",
+        "last_name": "Holy",
+        "full_name": "Jack Holy"},
+       {"first_name": "Mike",
+        "last_name": "Adams",
+        "full_name": "Mike Adams"}])]
+)
+def test_restore_names_right(first_in: list[dict],
+                             then_out: list[dict]) -> None:
+    restore_names(first_in)
+    assert first_in[0]["first_name"] == then_out[0]["first_name"]
+    assert first_in[1]["first_name"] == then_out[1]["first_name"]
